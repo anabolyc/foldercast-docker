f635b48e4cc1f52ba64aed85603d2eb73acc4821
andrey.malyshenko <andrey.malyshenko@gmail.com>
2021-04-30 11:00:25 +0200

    Escaped root folder in startup script

--- ices/start.sh
+++ ices/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-find $ROOT_FOLDER -iname "*.mp3" > /ices-0.4/playlist.txt && \
+find "$ROOT_FOLDER" -iname "*.mp3" > /ices-0.4/playlist.txt && \
+cat /ices-0.4/playlist.txt && \
 /ices-0.4/ices \
     -C 2 \
     -D /ices-0.4 \
